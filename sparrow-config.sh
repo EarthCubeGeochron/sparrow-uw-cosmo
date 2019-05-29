027315f86a563db2ae486600d91884015eaf1067
Daven Quinn <dev@davenquinn.com>
2019-05-29 12:44:47 -0500

    Updated Sparrow

--- sparrow-config.sh
+++ sparrow-config.sh
@@ -16,5 +16,6 @@ export SPARROW_COMMANDS="$pipeline/bin"
 export SPARROW_INIT_SQL="$pipeline/sql"
 
 export SPARROW_BASE_URL="/labs/cosmo-test"
+export SPARROW_DB_PORT="51428"
 export SPARROW_HTTP_PORT="50102"
 export SPARROW_COMPOSE_OVERRIDES="-f $project_dir/docker-compose.overrides.yaml"
