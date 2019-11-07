3715af38251971885fae539a62bc29d961836f38
Daven Quinn <dev@davenquinn.com>
2019-11-07 15:07:41 -0600

    Updated sparrow and config

--- sparrow-config.sh
+++ sparrow-config.sh
@@ -5,6 +5,8 @@ cosmo="$(git rev-parse --show-toplevel)"
 export SPARROW_PATH="$cosmo/Sparrow"
 
 export SPARROW_SECRET_KEY="OrangeTree2019222"
+export MAPBOX_API_TOKEN="pk.eyJ1IjoiZGF2ZW5xdWlubiIsImEiOiJjanZ1eWwxMjAwNmRvM3lzNTNqN2d0OHdzIn0.kmDqABs8gHCaihj8UdnQKg"
+
 export SPARROW_LAB_NAME="Cosmogenic Nuclides Lab"
 export COMPOSE_PROJECT_NAME="UWCosmo"
 
@@ -17,3 +19,4 @@ export SPARROW_COMMANDS="$pipeline/bin"
 export SPARROW_INIT_SQL="$pipeline/sql"
 
 export SPARROW_SITE_CONTENT="$cosmo/site-content"
+export SPARROW_PLUGINS="$cosmo/cosmo_api"
