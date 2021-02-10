c33f502178c0bb7670e84a9a457a1255402daa20
yeshancqcq <yeshan@umich.edu>
2021-02-09 19:59:31 -0600

    form

--- site-content/form.js
+++ site-content/form.js
@@ -1,7 +1,7 @@
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
+ * DS207: Consider shorter variations of null checkss
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 import React, { Component } from "react";
