09a1b406a14f388bd1df1332b2b8914dabeaa22f
yeshancqcq <yeshan@umich.edu>
2021-06-02 10:09:42 -0400

    db fields as plugins

---
+++ sql/extend-schema.sql
@@ -0,0 +1,10 @@
+
+ALTER TABLE sample ADD COLUMN thickness numeric;
+ALTER TABLE sample ADD COLUMN lab_name text;
+ALTER TABLE sample ADD COLUMN lab_date timestamp;
+ALTER TABLE sample ADD COLUMN lab_standard text;
+ALTER TABLE sample ADD COLUMN atm_pressure numeric;
+ALTER TABLE sample ADD COLUMN sample_name text;
+ALTER TABLE sample ADD COLUMN group_id text;
+ALTER TABLE sample ADD COLUMN sample_type text;
+ALTER TABLE sample ADD COLUMN pub_year numeric;
