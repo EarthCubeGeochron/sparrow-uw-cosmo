8017f9a161f0fbf4b59ac52b9b32e64fc436184c
yeshancqcq <yeshan@umich.edu>
2021-06-09 19:39:01 -0400

    extensions schema for sql

--- import_pipeline/sql/schema-extensions.sql
+++ import_pipeline/sql/schema-extensions.sql
@@ -1,5 +1,17 @@
 ALTER TABLE sample
 ADD COLUMN elevation numeric;
+ALTER TABLE sample
+ADD COLUMN lab_name text;
+ALTER TABLE sample
+ADD COLUMN lab_standard text;
+ALTER TABLE sample
+ADD COLUMN lab_date timestamp;
+ALTER TABLE sample
+ADD COLUMN thickness numeric;
+ALTER TABLE sample
+ADD COLUMN boulder_height numeric;
+ALTER TABLE sample
+ADD COLUMN atm_pressure numeric;
 
 ALTER TABLE analysis
 ADD COLUMN standard_id text;
